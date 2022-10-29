d8b6028ce217b5b2939b7506285d5e3e1b7a71e9
Edy Segura <edysegura@gmail.com>
2022-10-29 16:12:03 -0300

    feat: add poke data preload

--- server.js
+++ server.js
@@ -3,8 +3,31 @@ import Application from './src/Application'
 
 const context = Nullstack.start(Application)
 
+function cacheFirstGenerationData() {
+  const pokeList = []
+  for (let i = 1; i < 152; i++) {
+    pokeList.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
+  }
+  return pokeList
+}
+
+function cacheFirstGenerationSprites() {
+  const pokeSprites = []
+  for (let i = 1; i < 152; i++) {
+    pokeSprites.push(
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i}.png`,
+    )
+  }
+  return pokeSprites
+}
+
 context.start = async function start() {
   // https://nullstack.app/application-startup
+  const { worker } = context
+  worker.preload = [
+    ...cacheFirstGenerationData(),
+    ...cacheFirstGenerationSprites(),
+  ]
 }
 
 export default context
