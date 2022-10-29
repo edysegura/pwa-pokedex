80813f7cbc916659a84dff2c84fc12019271eae6
Edy Segura <edysegura@gmail.com>
2022-10-29 11:59:44 -0300

    chore: baseline for pokemon tag

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,9 +11,13 @@ class Home extends Nullstack {
       <section class="my-0 mx-auto flex min-h-screen w-full max-w-3xl flex-wrap items-center p-6 md:flex-nowrap">
         <h1>PWA Pokédex</h1>
         <img
+          width="250"
           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
           alt=""
         />
+        <span class="rounded-xl bg-rock py-1 px-2 text-xs font-bold text-white">
+          Rock
+        </span>
       </section>
     )
   }
